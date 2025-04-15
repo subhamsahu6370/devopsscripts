5721920bf3429fdb05fa464fa8d9a42b287a6d85
subhamsahu6370 <suvam6370@gmail.com>
2025-04-15 16:19:45 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -11,11 +11,11 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket reyaz-kops-testbkt123.k8s.local --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
-aws s3api put-bucket-versioning --bucket reyaz-kops-testbkt123.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://reyaz-kops-testbkt123.k8s.local
-kops create cluster --name reyaz.k8s.local --zones ap-south-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
-kops update cluster --name reyaz.k8s.local --yes --admin
+aws s3api create-bucket --bucket subham-kops-testbkt123.k8s.local --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
+aws s3api put-bucket-versioning --bucket subham-kops-testbkt123.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://subham-kops-testbkt123.k8s.local
+kops create cluster --name subham.k8s.local --zones ap-south-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
+kops update cluster --name subham.k8s.local --yes --admin
 
 #kops validate cluster --wait 10m
-#kops delete cluster --name reyaz.k8s.local --yes
+#kops delete cluster --name subham.k8s.local --yes
